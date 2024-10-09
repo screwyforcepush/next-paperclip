ee2c8efa2f4f98432350892127f038e4540ba03f
asavage <alex.savage@shinetech.com>
2024-10-09 22:11:20 +1100

    comments

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,6 +1,7 @@
 export const getServerSideEnv = () => ({
   OPENAI_API_KEY: process.env.LITELLM_API_KEY,
   API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  LITELLM_BASE_PATH: process.env.LITELLM_BASE_PATH,
 });
 
 export const getClientSideEnv = () => ({
--- src/lib/utils/openaiConfig.ts
+++ src/lib/utils/openaiConfig.ts
@@ -10,64 +10,18 @@ const DEFAULT_METADATA = {
   business_objective: "nobusiness_objective"
 };
 
-// properties = {
-//   "$session_id": clean_metadata.get("session_id"),
-//   "cost": kwargs.get("response_cost", 0),
-//   "prompt": messages,
-//   "system_message": extract_message_content(messages)[0],
-//   "last_message": extract_message_content(messages)[1],
-//   "response": extract_response_content(response_obj),
-//   "business_objective": clean_metadata.get("business_objective", None),
-//   "milestone": clean_metadata.get("milestone") or clean_metadata.get("trace_name", None),
-//   "milestone_step": clean_metadata.get("milestone_step") or clean_metadata.get("generation_name", None),
-//   "router_metadata": {
-//       "model": kwargs.get("model", ""),
-//       "response_time": response_time,
-//       "cache_hit": cache_hit,
-//       "call_type": call_type,
-//       "model_group": kwargs.get("model_group"),
-//       "tokens": usage,
-//       "router_user": kwargs.get("user", ""),
-//   },
-//   "timestamp": end_time.strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3] + "Z"  # ISO 8601 format with milliseconds
-//   }
-
-// posthog_user_id = clean_metadata.get("user_id") or clean_metadata.get("trace_user_id", None)  
-// event_name = clean_metadata.get("event_name") or clean_metadata.get("milestone_step") or clean_metadata.get("generation_name", "")
-// event_name = f"{event_name}_gen"
-// if posthog_user_id is None:
-//   properties['$process_person_profile']=False
-// capture_id = posthog_user_id or str(uuid.uuid4())
-
-// self.Posthog.capture(
-//   capture_id, 
-//   event_name, 
-//   {
-//       **properties
-//   }
-// )
-
-
-// gameId: gameState.gameId,
-// sessionId: gameState.sessionId,
-// userId: gameState.userId,
-// cycle: gameState.currentCycle,
-// kpis: gameState.kpiHistory[gameState.kpiHistory.length - 1] || {},
-// trace_id: uuidv4()
-
 export const getChatOpenAI = (() => {
   let instance: ChatOpenAI | null = null;
 
   return (metadata?: any) => {
     if (!instance) {
       const env = getServerSideEnv();
-      console.log("serversideenv", env);
       instance = new ChatOpenAI({
         openAIApiKey: env.OPENAI_API_KEY,
         modelName: "oaimini",
         temperature: 1.0,
         configuration: { 
-          basePath: "https://route.flowguard.app" // Use basePath instead of baseURL
+          basePath: env.LITELLM_BASE_PATH // Use basePath instead of baseURL
         },
         modelKwargs: {
           metadata: {}
@@ -76,11 +30,11 @@ export const getChatOpenAI = (() => {
     }
 
     const mappedMetadata = metadata ? Object.entries({
-      trace_name: metadata.cycle ? `Cycle: ${metadata.cycle}` : undefined,
+      trace_name: metadata.cycle ? `Cycle: ${metadata.cycle}` : undefined, //milestone
       trace_id: metadata.trace_id,
-      session_id: metadata.sessionId,
+      session_id: metadata.sessionId, 
       trace_user_id: metadata.userId,
-      generation_name: metadata.inferenceObjective,
+      generation_name: metadata.inferenceObjective, //milestone_step
       business_objective: metadata.gameId ? `Game ID: ${metadata.gameId}` : undefined,
       kpis: metadata.kpis //TODO handle this in litellm
     }).reduce((acc, [key, value]) => value !== undefined ? { ...acc, [key]: value } : acc, {}) : {};
